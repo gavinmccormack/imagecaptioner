6501c2460293faf984c759034031082339b2e106
TheGrimJam <gavin.mccormack@gmail.com>
2022-03-11 20:36:31 +0000

    Code primping.

--- captioner.py
+++ captioner.py
@@ -1,19 +1,19 @@
-# image_viewer.py
+#!/usr/bin/env python3
+"""
+Desktop utility for speeding through image captioning and template generation.
+
+Aim is for user friendly.
+"""
 import io
-import os, os.path
+import os
+import os.path
 import PySimpleGUI as sg
 from PIL import Image
 
-file_types = [("JPEG (*.jpg)", "*.jpg", "*.png"), ("All files (*.*)", "*.*")]
 
+VALID_IMAGES = [".jpg", ".gif", ".png", ".tga"]
 
-def main():
-    # Get list of images
-    imgs = []
-    path = os.getcwd()  # NB: Probably needs amending to be target dir.
-    target_path = os.path.join(path, "put-images-here")
-
-    latex_template = """
+LATEX_TEMPLATE = """
 \\begin{figure}
 \\caption{%(caption)s}
 \\begin{center}
@@ -21,34 +21,49 @@ def main():
 \\label{fig:%(filename)s}
 \\end{center}
 \\end{figure}
-            """
+"""
 
-    sg.theme("DarkTeal9")
-    if not os.path.exists(target_path):
-        os.mkdir(target_path)
-    valid_images = [".jpg", ".gif", ".png", ".tga"]
-    for f in os.listdir(target_path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() not in valid_images:
+
+PATH = os.getcwd()  # NB: Probably needs amending to be target dir.
+TARGET_PATH = os.path.join(PATH, "put-images-here")
+
+
+sg.theme("DarkTeal9")
+
+
+def get_list_of_image_paths() -> list:
+    """Gets.... a list of image paths! in the TARGET_PATH directory"""
+    if not os.path.exists(TARGET_PATH):
+        os.mkdir(TARGET_PATH)
+    imgs = []
+    for file in os.listdir(TARGET_PATH):
+        ext = os.path.splitext(file)[1]
+        if ext.lower() not in VALID_IMAGES:
             continue
-        imgs.append(os.path.join(target_path, f))
+        imgs.append(os.path.join(TARGET_PATH, file))
 
-    number_of_images = len(imgs)
-    count = 0
-    if number_of_images == 0:
-        print("You din't put any images in")
+    if len(imgs) == 0:
+        print("You didn't put any images in")
         exit()
+    return imgs
 
-    image_section = sg.Image(key="-IMAGE-")
+
+def main():
+    """Get list of images"""
+    imgs = get_list_of_image_paths()
+    number_of_images = len(imgs)
+    index = 0  # Index of current image
+
     layout = [
         [
             sg.Text(
-                "%s of %s" % (count, number_of_images),
+                f"{index} of {number_of_images}",
                 font="Helvetica",
-                key="Image-Count",
+                key="Image-index",
+                justification="left",
             )
         ],
-        [sg.Column([[image_section]], justification="center")],
+        [sg.Column([[sg.Image(key="-IMAGE-")]], justification="center")],
         [
             sg.Text("Input a caption"),
             sg.Multiline(
@@ -67,19 +82,22 @@ def main():
                 do_not_clear=True,
                 pad=(20, (15, 20)),
             ),
-            sg.Button("Add", pad=(0, (15, 0))),
+            sg.Button("Add", font="Helvetica", pad=(0, (15, 0))),
         ],
     ]
     window = sg.Window(
         "Captions! CAPTIONS! CAPTIONSSSSS!", layout, margins=(20, 20), finalize=True
     )
     window["-MLINE-"].bind("<Return>", "_Enter")
-    window["-MLINE2-"].update(latex_template)
+    window["-MLINE2-"].update(LATEX_TEMPLATE)
 
     while True:
-        filename = (
-            imgs.pop()
-        )  # This crashes the program when finished. But like, a cool crash.
+        try:
+            filename = imgs.pop()
+        except:
+            print("Done.")
+            break
+
         if os.path.exists(filename):
             image = Image.open(filename)
             image.thumbnail(
@@ -89,31 +107,33 @@ def main():
             image.save(bio, format="PNG")
             window["-IMAGE-"].update(data=bio.getvalue())
 
-            # Update counter text
-            count += 1
-            window["Image-Count"].update("%s of %s" % (count, number_of_images))
+            # Update indexer text
+            index += 1
+            window["Image-index"].update("{index} of {number_of_images}")
 
         event, values = window.read()
 
-        if event == "Exit" or event == sg.WIN_CLOSED:
+        if event in ["Exit", sg.WIN_CLOSED]:
             break
 
-        if event == "-MLINE-_Enter" or event == "Add":
+        if event in ["Add", "-MLINE-_Enter"]:
             caption = values["-MLINE-"]
 
             filename_without_ext = os.path.basename(filename).split(".")[0]
-            latex_template = values["-MLINE2-"] % {
+            output_latex = values["-MLINE2-"] % {
                 "caption": caption,
                 "filepath": filename,
                 "filename": filename_without_ext,
             }
+
             try:
-                with open("outputs.tex", "a") as file:
-                    print("Adding to file: \n", latex_template)
-                    file.write(latex_template)
+                with open("outputs.tex", "a", encoding="UTF-8") as file:
+                    print("Added to output file")
+                    file.write(output_latex)
             except:
+                print("Something went wrong with the file access")
                 print(
-                    "Something went wrong with the file. Check permissions, or create a 'outputs.tex' file in this directory"
+                    "Check permissions, or create the 'outputs.tex' file in this directory"
                 )
 
     window.close()
